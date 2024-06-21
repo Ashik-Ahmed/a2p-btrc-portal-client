66a226fa76c0e9346ec4d4d80fdd837a1bdd8529
ashik-ahmed <ashikahmed121@gmail.com>
2024-06-22 00:23:15 +0600

    project initialized with authentication

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl font-bold">Hey, Welcome</h1>
+      <p className="text-lg font-semibold">This is Homepage</p>
+    </div>
+  );
+}

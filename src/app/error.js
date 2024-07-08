fcb0f134941145227782eb2549578f39e3563498
ashik-ahmed <ashikahmed121@gmail.com>
2024-07-08 23:37:00 +0600

    error page modified

--- src/app/error.js
+++ src/app/error.js
@@ -4,9 +4,13 @@ import React from 'react'
 
 const ErrorPage = ({ error, reset }) => {
     return (
-        <div>
-            <h1>Error: {error.message}</h1>
-            <button onClick={() => reset()} className='bg-secondary p-2 rounded-md text-white'>Reset error boundary</button>
+        <div className='flex flex-col gap-4 justify-center items-center w-full h-[90vh]'>
+            <h1 className='text-3xl'>An Error Occurred</h1>
+            <i className="pi pi-exclamation-triangle text-9xl text-red"></i>
+            <button type="button" onClick={() => reset()} className='flex justify-between items-center gap-x-2 bg-blue-500 p-2 rounded-md text-white'>
+                <i className="pi pi-refresh"></i>
+                <span>Reset Error</span>
+            </button>
         </div>
     )
 }

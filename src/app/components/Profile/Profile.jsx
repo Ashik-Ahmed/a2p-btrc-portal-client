6a4325a66f9181c984299ee03876834c34f3d4e4
ashik-ahmed <ashikahmed121@gmail.com>
2024-07-13 23:25:37 +0600

    update password dialog with form design complete

---
+++ src/app/components/Profile/Profile.jsx
@@ -0,0 +1,196 @@
+"use client";
+
+import React, { useState } from 'react'
+import Image from 'next/image';
+import profilePic from '../../../../public/images/user.png';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FaCheck, FaLanguage, FaLocationDot, FaPhone, FaRegStar, FaRegUser, FaUserPen } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { Dialog } from 'primereact/dialog';
+
+const Profile = ({ user }) => {
+    const [updatePasswordDialog, setUpdatePasswordDialog] = useState(false);
+    const [showCurrentPassword, setShowCurrentPassword] = useState(false);
+    const [showNewPassword, setShowNewPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+
+    return (
+        <div className="max-w-6xl mx-auto">
+            {/* Header */}
+            <div className="relative flex items-center h-48 bg-gradient-to-r from-teal-300 via-pink-300 to-yellow-300 rounded-t-md">
+                <div className="md:flex items-center absolute top-36 w-full bg-white h-24 rounded-b-md border-white shadow-lg">
+                    <div className='flex justify-center items-center mx-auto md:mx-0 md:ml-6 -mt-18 w-30 h-30 border-4 border-white rounded-md bg-white'>
+                        <Image src={user?.data?.photo ? user?.data?.photo : profilePic} width={100} height={100} alt="Profile Picture" className="rounded w-28 h-28" />
+                    </div>
+                    <div className='mt-2 md:-mt-12 md:ml-6'>
+                        <h3 className='text-2xl font-semibold text-gray-600 text-center'>{user?.data?.name}</h3>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className="flex items-center justify-start gap-x-2 w-full mt-16">
+                <button className="flex items-center gap-x-2 bg-primary/90 hover:bg-primary text-white text-sm py-1 px-1.5 rounded ">
+                    <FaUserPen size={18} />
+                    Edit Profile
+                </button>
+                <button onClick={() => setUpdatePasswordDialog(true)} className="flex items-center gap-x-2 bg-primary/90 hover:bg-primary text-white text-sm py-1 px-1.5 rounded">
+                    <RiLockPasswordLine size={18} />
+                    Update Password
+                </button>
+            </div>
+
+            {/* Main Content */}
+
+            <div className="flex flex-col md:flex-row md:space-x-6 mt-6">
+                {/* Profile Info */}
+                <div className="w-full md:w-1/3 bg-white p-4 rounded-md shadow-lg">
+                    <div className="rounded-lg">
+                        <h3 className="text-lg font-light text-gray-400">ABOUT</h3>
+                        <div className="flex gap-x-2 items-center mt-2 text-gray-600">
+                            <FaRegUser size={18} />
+                            <p className='font-semibold'>Full Name:</p>
+                            <p> {user?.data?.name}</p>
+                        </div>
+                        <div className="flex gap-x-2 items-center mt-2 text-gray-600">
+                            <FaCheck size={18} />
+                            <p className='font-semibold'>Status:</p>
+                            <p> {user?.data?.status}</p>
+                        </div>
+                        <div className="flex gap-x-2 items-center mt-2 text-gray-600">
+                            <FaRegStar size={18} />
+                            <p className='font-semibold'>Role:</p>
+                            <p> {user?.data?.role}</p>
+                        </div>
+                        <div className="flex gap-x-2 items-center mt-2 text-gray-600">
+                            <FaLanguage size={18} />
+                            <p className='font-semibold'>Language:</p>
+                            <p> {user?.data?.language || "Bengali, English"}</p>
+                        </div>
+
+                        <h3 className="text-lg font-light text-gray-400 mt-8">CONTACT</h3>
+                        <div className="flex gap-x-2 items-center mt-2 text-gray-600">
+                            <FaPhone size={18} />
+                            <p className='font-semibold'>Phone:</p>
+                            <p> {user?.data?.phone}</p>
+                        </div>
+                        <div className="flex gap-x-2 items-center mt-2 text-gray-600">
+                            <MdOutlineEmail size={20} />
+                            <p className='font-semibold'>Email:</p>
+                            <p> {user?.data?.email}</p>
+                        </div>
+                        <div className="flex gap-x-2 items-center mt-2 text-gray-600">
+                            <FaLocationDot size={18} />
+                            <p className='font-semibold'>Address:</p>
+                            <p> {user?.data?.address}</p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Activity Timeline */}
+                <div className="w-full md:w-2/3 mt-6 md:mt-0 bg-white p-4 rounded-md shadow-md">
+                    <h3 className="text-lg font-light text-gray-400">BIO</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis molestiae modi officia, consequuntur animi similique repellendus. Quam, magnam animi. Magnam quisquam molestiae laborum autem! Ullam ex optio inventore fuga expedita.
+                        Maxime alias fugit recusandae consectetur eius nemo enim minima aspernatur necessitatibus rerum? Voluptas reiciendis eaque labore facere suscipit sapiente rerum maxime porro quaerat! Consequatur, ea sit eum facilis eaque dolore.</p>
+                </div>
+            </div>
+            <Dialog header="Update Password" visible={updatePasswordDialog} onHide={() => { if (!updatePasswordDialog) return; setUpdatePasswordDialog(false); }}
+                style={{ width: '50vw', margin: '1rem' }} breakpoints={{ '960px': '75vw', '641px': '100vw' }}>
+                <form>
+                    <div className="mb-4 relative">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="current-password">
+                            Current Password
+                        </label>
+                        <div className="relative">
+                            <input
+                                id='current-password'
+                                type={showCurrentPassword ? 'text' : 'password'}
+                                placeholder="Enter Current password"
+                                className="w-full rounded border border-stroke bg-transparent py-1.5 pl-4 pr-8 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                            />
+                            <button
+                                type="button"
+                                onClick={() => setShowCurrentPassword(!showCurrentPassword)}
+                                className="absolute right-2 top-2 focus:outline-none"
+                            >
+                                {showCurrentPassword ? (
+                                    <FaEyeSlash className="h-5 w-5 text-gray-600" />
+                                ) : (
+                                    <FaEye className="h-5 w-5 text-gray-600" />
+                                )}
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="mb-4 relative">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="new-password">
+                            New Password
+                        </label>
+                        <div className="relative">
+                            <input
+                                id='new-password'
+                                type={showNewPassword ? 'text' : 'password'}
+                                placeholder="Enter new password"
+                                className="w-full rounded border border-stroke bg-transparent py-1.5 pl-4 pr-8 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                            />
+                            <button
+                                type="button"
+                                onClick={() => setShowNewPassword(!showNewPassword)}
+                                className="absolute right-2 top-2 focus:outline-none"
+                            >
+                                {showNewPassword ? (
+                                    <FaEyeSlash className="h-5 w-5 text-gray-600" />
+                                ) : (
+                                    <FaEye className="h-5 w-5 text-gray-600" />
+                                )}
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="mb-6 relative">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirm-password">
+                            Confirm New Password
+                        </label>
+                        <div className="relative">
+                            <input
+                                id='confirm-password'
+                                type={showConfirmPassword ? 'text' : 'password'}
+                                placeholder="Confirm new password"
+                                className="w-full rounded border border-stroke bg-transparent py-1.5 pl-4 pr-8 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                            />
+
+
+
+                            <button
+                                type="button"
+                                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                                className="absolute right-2 top-2 focus:outline-none"
+                            >
+                                {showConfirmPassword ? (
+                                    <FaEyeSlash className="h-5 w-5 text-gray-600" />
+                                ) : (
+                                    <FaEye className="h-5 w-5 text-gray-600" />
+                                )}
+                            </button>
+
+                        </div>
+                    </div>
+
+                    <div className="flex items-center justify-between">
+                        <button
+                            type="submit"
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring"
+                        >
+                            Change Password
+                        </button>
+                    </div>
+                </form>
+            </Dialog>
+        </div>
+    )
+}
+
+export default Profile
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,9 +1,6 @@
-import Image from 'next/image';
-import profilePic from '../../../public/images/user.png'; // replace with your actual profile image
-import { FaCheck, FaLanguage, FaLocationDot, FaPhone, FaRegStar, FaRegUser, FaUserPen } from "react-icons/fa6";
-import { MdOutlineEmail } from "react-icons/md";
-import { RiLockPasswordLine } from "react-icons/ri";
+
 import { auth } from '@/auth';
+import Profile from '../components/Profile/Profile';
 
 
 
@@ -11,90 +8,11 @@ export default async function ProfilePage() {
 
     const session = await auth();
     const user = await fetch(`http://localhost:5000/api/v1/user/${session?.user?._id}`).then(res => res.json());
-    console.log("user: ", user?.data);
+    // console.log("user: ", user?.data);
 
     return (
         <div>
-            <div className="max-w-6xl mx-auto">
-                {/* Header */}
-                <div className="relative flex items-center h-48 bg-gradient-to-r from-teal-300 via-pink-300 to-yellow-300 rounded-t-md">
-                    <div className="md:flex items-center absolute top-36 w-full bg-white h-24 rounded-b-md border-white shadow-lg">
-                        <div className='flex justify-center items-center mx-auto md:mx-0 md:ml-6 -mt-18 w-30 h-30 border-4 border-white rounded-md bg-white'>
-                            <Image src={user?.data?.photo || profilePic} width={100} height={100} alt="Profile Picture" className="rounded w-28 h-28" />
-                        </div>
-                        <div className='mt-2 md:-mt-12 md:ml-6'>
-                            <h3 className='text-2xl font-semibold text-gray-600 text-center'>{user?.data?.name}</h3>
-                        </div>
-                    </div>
-                </div>
-
-
-
-                <div className="flex items-center justify-start gap-x-2 w-full mt-16">
-                    <button className="flex items-center gap-x-2 bg-primary/90 hover:bg-primary text-white text-sm py-1 px-1.5 rounded ">
-                        <FaUserPen size={18} />
-                        Edit Profile</button>
-                    <button className="flex items-center gap-x-2 bg-primary/90 hover:bg-primary text-white text-sm py-1 px-1.5 rounded">
-                        <RiLockPasswordLine size={18} />
-                        Update Password</button>
-                </div>
-
-                {/* Main Content */}
-
-                <div className="flex flex-col md:flex-row md:space-x-6 mt-6">
-                    {/* Profile Info */}
-                    <div className="w-full md:w-1/3 bg-white p-4 rounded-md shadow-lg">
-                        <div className="rounded-lg">
-                            <h3 className="text-lg font-light text-gray-400">ABOUT</h3>
-                            <div className="flex gap-x-2 items-center mt-2 text-gray-600">
-                                <FaRegUser size={18} />
-                                <p className='font-semibold'>Full Name:</p>
-                                <p> {user?.data?.name}</p>
-                            </div>
-                            <div className="flex gap-x-2 items-center mt-2 text-gray-600">
-                                <FaCheck size={18} />
-                                <p className='font-semibold'>Status:</p>
-                                <p> {user?.data?.status}</p>
-                            </div>
-                            <div className="flex gap-x-2 items-center mt-2 text-gray-600">
-                                <FaRegStar size={18} />
-                                <p className='font-semibold'>Role:</p>
-                                <p> {user?.data?.role}</p>
-                            </div>
-                            <div className="flex gap-x-2 items-center mt-2 text-gray-600">
-                                <FaLanguage size={18} />
-                                <p className='font-semibold'>Language:</p>
-                                <p> {user?.data?.language || "Bengali, English"}</p>
-                            </div>
-
-                            <h3 className="text-lg font-light text-gray-400 mt-8">CONTACT</h3>
-                            <div className="flex gap-x-2 items-center mt-2 text-gray-600">
-                                <FaPhone size={18} />
-                                <p className='font-semibold'>Phone:</p>
-                                <p> {user?.data?.phone}</p>
-                            </div>
-                            <div className="flex gap-x-2 items-center mt-2 text-gray-600">
-                                <MdOutlineEmail size={20} />
-                                <p className='font-semibold'>Email:</p>
-                                <p> {user?.data?.email}</p>
-                            </div>
-                            <div className="flex gap-x-2 items-center mt-2 text-gray-600">
-                                <FaLocationDot size={18} />
-                                <p className='font-semibold'>Address:</p>
-                                <p> {user?.data?.address}</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Activity Timeline */}
-                    <div className="w-full md:w-2/3 mt-6 md:mt-0 bg-white p-4 rounded-md shadow-md">
-                        <h3 className="text-lg font-light text-gray-400">BIO</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis molestiae modi officia, consequuntur animi similique repellendus. Quam, magnam animi. Magnam quisquam molestiae laborum autem! Ullam ex optio inventore fuga expedita.
-                            Maxime alias fugit recusandae consectetur eius nemo enim minima aspernatur necessitatibus rerum? Voluptas reiciendis eaque labore facere suscipit sapiente rerum maxime porro quaerat! Consequatur, ea sit eum facilis eaque dolore.</p>
-                    </div>
-                </div>
-
-            </div>
+            <Profile user={user} />
         </div>
     );
 }

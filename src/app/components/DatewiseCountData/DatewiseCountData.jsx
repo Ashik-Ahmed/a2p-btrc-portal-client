9055d93d24a1c73c3760f3c1b6d3d43fb8b433dc
ashik-ahmed <ashikahmed121@gmail.com>
2024-07-24 23:12:36 +0600

    showing formatted number count for datewise report table data

--- src/app/components/DatewiseCountData/DatewiseCountData.jsx
+++ src/app/components/DatewiseCountData/DatewiseCountData.jsx
@@ -8,6 +8,7 @@ import { getDatewiseCountReport } from '@/app/serverActions/report';
 import { Button } from 'primereact/button';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import formatNumberBD from '@/utils/numberFormat';
 
 
 const DatewiseCountData = ({ accessToken }) => {
@@ -33,12 +34,24 @@ const DatewiseCountData = ({ accessToken }) => {
         setLoading(false);
     }
 
+    const dippingCountBodyTemplate = (rowData) => {
+        return (
+            <span>{formatNumberBD(rowData?.dipping_count)}</span>
+        );
+    }
+
+    const smsCountBodyTemplate = (rowData) => {
+        return (
+            <span>{formatNumberBD(rowData?.sms_count)}</span>
+        );
+    }
+
     return (
         <div>
             <div className='px-4 py-2 my-2 bg-white rounded'>
                 <h2 className='uppercase text-xl font-light text-graydark'>Filter Options</h2>
-                <form onSubmit={handleSubmit(getDatewiseCount)} className='mt-8 flex gap-x-2'>
-                    <FloatLabel className="w-1/5 mb-2">
+                <form onSubmit={handleSubmit(getDatewiseCount)} className='mt-8 md:flex md: items-center gap-x-2'>
+                    <FloatLabel className="w-full md:w-fit mb-2 md:mb-0">
                         <Controller
                             name="filterDate"
                             control={control}
@@ -64,7 +77,7 @@ const DatewiseCountData = ({ accessToken }) => {
                         {errors.filterDate && <span className='text-xs text-red' role="alert">{errors.filterDate.message}</span>}
                     </FloatLabel>
 
-                    <Button type='submit' label='Search' loading={loading} className="bg-sky-500 text-white p-2 rounded" />
+                    <Button type='submit' label='Search' size='small' loading={loading} className="bg-sky-500 text-white w-full md:w-fit p-2 rounded" />
 
                 </form>
 
@@ -104,8 +117,8 @@ const DatewiseCountData = ({ accessToken }) => {
                 <DataTable value={reportData} size="small" paginator rows={10} rowsPerPageOptions={[5, 10, 25, 50]}
                     emptyMessage="No data found" loading={loading} className="custom-header report-table">
                     <Column field="delivery_date" header="Delivery Date" />
-                    <Column field="dipping_count" header="Dipping Count" />
-                    <Column field="sms_count" header="SMS Count" />
+                    <Column body={dippingCountBodyTemplate} header="Dipping Count" />
+                    <Column body={smsCountBodyTemplate} header="SMS Count" />
                 </DataTable>
             </div>
         </div >
